d70ff7aba8c97ddfb2e1f84bcb704ae552648f34
dutertk <dutertk@hotmail.com>
2018-11-28 15:02:49 +0800

    AddTwoSum

---
+++ src/demo/AddSumSolutionTest.java
@@ -0,0 +1,19 @@
+package demo;
+
+import solution.AddSumSolution;
+
+public class AddSumSolutionTest {
+	public static void main(String args[]) {
+		AddSumSolution addSumSolution = new AddSumSolution();
+		int[] nums = {3,3};
+		int	target = 6;
+
+		int[] index = addSumSolution.twoSum(nums, target);
+		for(Integer indexNumber : index) {
+			System.out.println(indexNumber);
+			
+		}
+		
+	}
+	
+}
---
+++ src/solution/AddSumSolution.java
@@ -0,0 +1,23 @@
+package solution;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class AddSumSolution {
+	
+	public int[] twoSum(int[] nums, int target) {
+		
+		
+		int[] returnIndex = new int[2];
+		for(int i = 0; i <= nums.length-1;i++) {
+			for (int j = i+1; j <= nums.length-1;j++) {
+				if(nums[i] + nums[j] == target) {
+					returnIndex[0] = i;
+					returnIndex[1] = j;
+					break;
+				}
+			}
+		}
+		return returnIndex;
+	}
+}
